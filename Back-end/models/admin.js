445b17cd7749ec0cb93bfde28c6856ae41449a2d
chiluverugirish <girishchieveru@gmail.com>
2026-04-30 14:34:50 +0530

    changes in models

--- Back-end/models/admin.js
+++ Back-end/models/admin.js
@@ -1,20 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const adminSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true,
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//     },
-//     role: { type: String, default: 'admin' },
-// });
-
-// const Admin = mongoose.model('admin', adminSchema);
-
-// module.exports = Admin;
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
