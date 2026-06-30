81287bc6b2746f349fd28a79c35ec153df59fbae
chiluverugirish <girishchieveru@gmail.com>
2026-07-01 02:44:30 +0530

    infra: Add ECR repository resources to Terraform

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -21,11 +21,29 @@ module "vpc" {
 
 # 2. EC2 Module (Handles Public EC2, Private EC2, Security Groups, Elastic IPs)
 module "ec2" {
-  source             = "./modules/ec2"
-  environment        = var.environment
-  vpc_id             = module.vpc.vpc_id
-  public_subnet_ids  = module.vpc.public_subnet_ids
-  private_subnet_ids = module.vpc.private_subnet_ids
-  instance_type      = var.instance_type
-  key_name           = var.key_name
+  source                = "./modules/ec2"
+  environment           = var.environment
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_ids     = module.vpc.public_subnet_ids
+  private_subnet_ids    = module.vpc.private_subnet_ids
+  public_instance_type  = var.public_instance_type
+  private_instance_type = var.private_instance_type
+  key_name              = var.key_name
+}
+
+# 3. ECR Repositories for Docker images
+resource "aws_ecr_repository" "backend" {
+  name                 = "${var.environment}-emotilearn-backend"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "ml" {
+  name                 = "${var.environment}-emotilearn-ml"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
